8a3df2ed46fa3bb7f8d0229016b147100b95e0f3
vision71tech <zaidbinasim2197@gmail.com>
2026-09-14 11:23:22 +0500

    UI changes3

--- vite.config.ts
+++ vite.config.ts
@@ -12,4 +12,9 @@ export default defineConfig({
     // nitro/vite builds from this
     server: { entry: "server" },
   },
+  vite: {
+    ssr: {
+      noExternal: ['tslib', '@radix-ui/*'],
+    },
+  },
 });
