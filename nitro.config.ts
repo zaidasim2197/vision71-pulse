6822e70353f829d63d5a6369ed6ef2305a4687ba
vision71tech <zaidbinasim2197@gmail.com>
2026-09-14 11:58:12 +0500

    fix Vercel server bundle

--- nitro.config.ts
+++ nitro.config.ts
@@ -1,19 +1,5 @@
 export default {
-  // Always build for Vercel serverless deployment
   preset: 'vercel',
-
-  // Inline tslib and radix-ui dependencies into server bundle
-  externals: {
-    inline: [/^tslib/, /^@radix-ui\//, 'clsx', 'tailwind-merge'],
-    traceInclude: [
-      'node_modules/tslib/**',
-      'tslib/**',
-    ],
-  },
-
-  // Node compatibility for Vercel
   node: true,
-
-  // Build settings
   minify: true,
-};
+}
